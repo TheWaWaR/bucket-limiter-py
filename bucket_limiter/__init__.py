cba0f9510b5fff06d4194f1046fc44b101f3dbf8
thewawar <thewawar@gmail.com>
2017-11-04 18:50:06 +0800

    Support multiple keys consume in one redis request

--- bucket_limiter/__init__.py
+++ bucket_limiter/__init__.py
@@ -1,85 +1,121 @@
 # coding: utf-8
 
 import time
+import hashlib
 
 import redis
+from redis.exceptions import RedisError
 
 LUA_SCRIPT = '''
-local key = KEYS[1]
-local interval = tonumber(ARGV[1])
-local capacity = tonumber(ARGV[2])
-local nTokens = tonumber(ARGV[3])
-local timeNow = tonumber(ARGV[4])
-local expire = tonumber(ARGV[5])
+local effects = {}
+for idx, key in ipairs(KEYS) do
+  local idxBase = (idx - 1) * 5
+  local interval = tonumber(ARGV[idxBase + 1])
+  local capacity = tonumber(ARGV[idxBase + 2])
+  local nTokens = tonumber(ARGV[idxBase + 3])
+  local timeNow = tonumber(ARGV[idxBase + 4])
+  local expire = tonumber(ARGV[idxBase + 5])
+  local currentTokens = -1
+  local lastFillAt = timeNow
 
-local currentTokens
-
-if redis.call('exists', key) == 0 then
-  currentTokens = capacity
-  redis.call('hset', key, 'lastFillAt', timeNow)
-else
-  local tokens = tonumber(redis.call('hget', key, 'tokens'))
-  local lastFillAt = tonumber(redis.call('hget', key, 'lastFillAt'))
-  if timeNow - lastFillAt > interval then
+  if redis.call('exists', key) == 0 then
     currentTokens = capacity
     redis.call('hset', key, 'lastFillAt', timeNow)
   else
-    currentTokens = tokens
+    lastFillAt = tonumber(redis.call('hget', key, 'lastFillAt'))
+    if timeNow - lastFillAt > interval then
+      currentTokens = capacity
+      redis.call('hset', key, 'lastFillAt', timeNow)
+    else
+      currentTokens = tonumber(redis.call('hget', key, 'tokens'))
+    end
+  end
+
+  assert(currentTokens >= 0)
+
+  if expire > 0 then
+    redis.call('expire', key, expire)
+  end
+
+  if nTokens > currentTokens then
+    redis.call('hset', key, 'tokens', currentTokens)
+    for i, effect in ipairs(effects) do
+      redis.call('hset', effect[1], 'tokens', effect[2])
+    end
+    return {key, interval, capacity, currentTokens, lastFillAt}
+  else
+    table.insert(effects, {key, currentTokens, nTokens})
   end
 end
 
-assert(currentTokens >= 0)
-
-if expire > 0 then
-  redis.call('expire', key, expire)
+for i, effect in ipairs(effects) do
+  redis.call('hset', effect[1], 'tokens', effect[2] - effect[3])
 end
 
-if nTokens > currentTokens then
-  redis.call('hset', key, 'tokens', currentTokens)
-  return 0
-else
-  redis.call('hset', key, 'tokens', currentTokens - nTokens)
-  return 1
-end
+return {'', 0, 0, 0, 0}
 '''
+LUA_SCRIPT_SHA1 = hashlib.sha1(LUA_SCRIPT).hexdigest()
 
 
 def now_ms():
     return int(time.time() * 1000)
 
 
-class Limiter(object):
+class RedisConsumeDenied(object):
+    def __init__(self, redis_rv):
+        self.redis_key = redis_rv[0]
+        self.interval = redis_rv[1] / 1000
+        self.capacity = redis_rv[2]
+        self.current_tokens = redis_rv[3]
+        self.last_fill_at = redis_rv[4]
 
-    def get_token_count(self, key):
-        raise NotImplemented()
+    def __repr__(self):
+        return '<RedisConsumeDenied([{}] interval={}, capacity={}, tokens={})>'.format(
+            self.redis_key, self.interval, self.capacity, self.current_tokens,
+        )
 
-    def consume(self, key, interval, capacity, n=1):
-        raise NotImplemented()
 
-
-class RedisLimiter(Limiter):
+class RedisLimiter(object):
 
     def __init__(self,
                  redis_cli=None,
-                 host='localhost', port=6379, db=0):
+                 host='localhost', port=6379, db=0,
+                 key_prefix='limiter'):
         if not redis_cli:
             redis_cli = redis.StrictRedis(host=host, port=port, db=db)
         self.redis_cli = redis_cli
+        self.key_prefix = key_prefix
 
     def get_redis_key(self, key, interval):
-        return 'limiter:{}:{}'.format(key, interval)
+        return '{}:{}:{}'.format(self.key_prefix, key, interval)
 
     def get_token_count(self, key, interval):
         redis_key = self.get_redis_key(key, interval)
         return self.redis_cli.hget(redis_key, 'tokens')
 
-    def consume(self, key, interval, capacity, n=1, expire=0):
-        redis_key = self.get_redis_key(key, interval)
-        if expire == 0:
-            expire = interval * 3 + 60
-        interval_ms = interval * 1000
-        return 1 == self.redis_cli.eval(
-            LUA_SCRIPT, 1,
-            redis_key,
-            interval_ms, capacity, n, now_ms(), expire
-        )
+    def consume(self, args):
+        script_keys = []
+        script_args = []
+        the_now_ms = now_ms()
+        for (key, interval, capacity, n) in args:
+            redis_key = self.get_redis_key(key, interval)
+            expire = interval * 2 + 15
+            interval_ms = interval * 1000
+            script_keys.append(redis_key)
+            script_args.extend([interval_ms, capacity, n, the_now_ms, expire])
+
+        for i in range(3):
+            try:
+                rv = self.redis_cli.evalsha(
+                    LUA_SCRIPT_SHA1, len(script_keys), *(script_keys + script_args)
+                )
+                if rv == ['', 0, 0, 0, 0]:
+                    return True, None
+                else:
+                    return False, RedisConsumeDenied(rv)
+            except RedisError:
+                sha1 = self.redis_cli.script_load(LUA_SCRIPT)
+                assert sha1 == LUA_SCRIPT_SHA1
+
+    def consume_one(self, key, interval, capacity, n=1):
+        return self.consume([(key, interval, capacity, n)])
--- limiter.lua
+++ limiter.lua
@@ -1,37 +1,47 @@
 
-local key = KEYS[1]
-local interval = tonumber(ARGV[1])
-local capacity = tonumber(ARGV[2])
-local nTokens = tonumber(ARGV[3])
-local timeNow = tonumber(ARGV[4])
-local expire = tonumber(ARGV[5])
+local effects = {}
+for idx, key in ipairs(KEYS) do
+  local idxBase = (idx - 1) * 5
+  local interval = tonumber(ARGV[idxBase + 1])
+  local capacity = tonumber(ARGV[idxBase + 2])
+  local nTokens = tonumber(ARGV[idxBase + 3])
+  local timeNow = tonumber(ARGV[idxBase + 4])
+  local expire = tonumber(ARGV[idxBase + 5])
+  local currentTokens = -1
+  local lastFillAt = timeNow
 
-local currentTokens
-
-if redis.call('exists', key) == 0 then
-  currentTokens = capacity
-  redis.call('hset', key, 'lastFillAt', timeNow)
-else
-  local tokens = tonumber(redis.call('hget', key, 'tokens'))
-  local lastFillAt = tonumber(redis.call('hget', key, 'lastFillAt'))
-  if timeNow - lastFillAt > interval then
+  if redis.call('exists', key) == 0 then
     currentTokens = capacity
     redis.call('hset', key, 'lastFillAt', timeNow)
   else
-    currentTokens = tokens
+    lastFillAt = tonumber(redis.call('hget', key, 'lastFillAt'))
+    if timeNow - lastFillAt > interval then
+      currentTokens = capacity
+      redis.call('hset', key, 'lastFillAt', timeNow)
+    else
+      currentTokens = tonumber(redis.call('hget', key, 'tokens'))
+    end
+  end
+
+  assert(currentTokens >= 0)
+
+  if expire > 0 then
+    redis.call('expire', key, expire)
+  end
+
+  if nTokens > currentTokens then
+    redis.call('hset', key, 'tokens', currentTokens)
+    for i, effect in ipairs(effects) do
+      redis.call('hset', effect[1], 'tokens', effect[2])
+    end
+    return {key, interval, capacity, currentTokens, lastFillAt}
+  else
+    table.insert(effects, {key, currentTokens, nTokens})
   end
 end
 
-assert(currentTokens >= 0)
-
-if expire > 0 then
-  redis.call('expire', key, expire)
+for i, effect in ipairs(effects) do
+  redis.call('hset', effect[1], 'tokens', effect[2] - effect[3])
 end
 
-if nTokens > currentTokens then
-  redis.call('hset', key, 'tokens', currentTokens)
-  return 0
-else
-  redis.call('hset', key, 'tokens', currentTokens - nTokens)
-  return 1
-end
+return {'', 0, 0, 0, 0}
